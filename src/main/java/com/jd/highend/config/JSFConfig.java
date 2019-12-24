e567e387b8e2bb5123b550db4d90b5c79c74a14f
wangrufei3 <wangrufei3@jd.com>
2019-12-24 16:07:45 +0800

    hhhh

--- src/main/java/com/jd/highend/config/JSFConfig.java
+++
@@ -1,9 +0,0 @@
-package com.jd.highend.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
-
-@Configuration
-@ImportResource(locations = "classpath:config/spring-jd.xml")
-public class JSFConfig {
-}
